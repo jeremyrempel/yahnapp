2d35e0a06efeed6a183bf444cb6f68ef326de118
Jeremy Rempel <jeremyrempel@gmail.com>
2020-09-25 23:28:00 -0400

    fetch from api, add timber

---
+++ libapi/src/main/java/com/github/jeremyrempel/yahnapp/api/HackerNewsApi.kt
@@ -0,0 +1,40 @@
+package com.github.jeremyrempel.yahnapp.api
+
+import io.ktor.client.HttpClient
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logger
+import io.ktor.client.features.logging.Logging
+import io.ktor.client.request.get
+import kotlinx.serialization.json.Json
+
+class HackerNewsApi(
+    private val scheme: String = "https",
+    private val host: String = "hacker-news.firebaseio.com"
+) {
+    private val client = HttpClient {
+        install(JsonFeature) {
+            val config = Json.Default
+            serializer = KotlinxSerializer(config)
+        }
+        install(Logging) {
+            logger = object : Logger {
+                override fun log(message: String) {
+                    // todo implement logging
+                    println("Network: $message")
+                }
+            }
+
+            level = LogLevel.INFO
+        }
+    }
+
+    suspend fun fetchItem(id: Int): Item {
+        return client.get(
+            scheme = scheme,
+            host = host,
+            path = "/v0/item/${id}.json"
+        )
+    }
+}
