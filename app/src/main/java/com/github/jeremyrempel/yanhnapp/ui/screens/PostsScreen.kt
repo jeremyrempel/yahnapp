241ca7f6ba056f3fccddf318cdefc20dae88cce4
Jeremy Rempel <jeremyrempel@gmail.com>
2020-10-03 10:15:41 -0400

    Update browser share state to resolve deprecated method in 1.3.0-alphha06

--- app/src/main/java/com/github/jeremyrempel/yanhnapp/ui/screens/PostsScreen.kt
+++ app/src/main/java/com/github/jeremyrempel/yanhnapp/ui/screens/PostsScreen.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.net.Uri
 import android.text.format.DateUtils
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.browser.customtabs.CustomTabsIntent.SHARE_STATE_ON
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.clickable
@@ -130,7 +131,7 @@ fun ListContent(
 fun launchBrowser(url: String, context: Context) {
     CustomTabsIntent.Builder()
         .setShowTitle(true)
-        .setDefaultShareMenuItemEnabled(true)
+        .setShareState(SHARE_STATE_ON)
         .setUrlBarHidingEnabled(true)
         .build()
         .launchUrl(context, Uri.parse(url))
